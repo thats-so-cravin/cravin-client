ea0240302e0cffc469b33b2c8cf450ffd748040b
Tara Johnson <tara@madface.com>
2018-06-28 12:26:38 -0700

    Link to source recipe working

--- scripts/views/search-view.js
+++ scripts/views/search-view.js
@@ -1,7 +1,6 @@
 'use strict';
 
-
-//Lisener for Sign Up button
+//Listener for Sign Up button
 $('#search').on('click', handleSearch);
 
 function handleSearch(event) {
@@ -12,10 +11,9 @@ function handleSearch(event) {
   searchData.third_ing = $('#third-ing').val();
   searchData.fourth_ing = $('#fourth-ing').val();
   let searchStr = `&q=${searchData.first_ing}+${searchData.second_ing}+${searchData.third_ing}+${searchData.fourth_ing}`;
+  console.log(`search str`, searchStr)
   $.get(`http://api.yummly.com/v1/api/recipes?_app_id=78c6217b&_app_key=084d45aa3306778e2ebbc3148fdaab96${searchStr}`, data => {console.log(data)})
 
+  // DONE: Call fetchAll and pass it searchStr, modify fetchAll to accept argument of searchStr
   app.Recipe.fetchAll(app.Recipe.loadAll, searchStr);
-
-  // call fetchall and pass it searchstr
-  // modify fetchall to accept arg of searchstr
 }
