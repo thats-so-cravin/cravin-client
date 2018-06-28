dc897e0459492d4d63c258ec3b694a6ba398e928
Tara Johnson <tara@madface.com>
2018-06-28 15:02:18 -0700

    Separating user sign up and sign in in progress

--- scripts/views/search-view.js
+++ scripts/views/search-view.js
@@ -6,10 +6,16 @@ function showSearchAndRecipes() {
 }
 
 //Listener for Sign Up button
-$('#search').on('click', handleSearch);
+$('#search-button').on('click', handleSearch);
 
 function handleSearch(event) {
   event.preventDefault();
+
+  // get request for current user
+  // if there are allergy/dietary strings, append them to the search
+  // add variables for allergy/diet strings
+  // else continue with search
+
   let searchData = {};
   searchData.first_ing = $('#first-ing').val();
   searchData.second_ing = $('#second-ing').val();
