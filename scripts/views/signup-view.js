849b8ec32ecf6780f20ae21e54a73dc2db9425ac
Suzanne Leber <79wabvlw1dt7@opayq.com>
2018-06-28 16:05:54 -0700

    Created seperate log in and sign up elements

--- scripts/views/signup-view.js
+++ scripts/views/signup-view.js
@@ -3,34 +3,7 @@
 //Lisener for Sign Up button
 $('#sign-up-button').on('click', handleForm);
 
-// CHANGE-TODO: Probably want to delete these at some point if we don't use
-
-// const allowedAllergy = {
-//   dairy: {description: 'Dairy-Free', id: 396},
-//   egg: {description: 'Egg-Free', id: 397},
-//   gluten: {description: 'Gluten-Free', id: 393},
-//   peanut: {description: 'Peanut-Free', id: 394},
-//   seafood: {description: 'Seafood-Free', id: 398},
-//   sesame: {description: 'Sesame-Free', id: 399},
-//   soy: {description: 'Soy-Free', id: 400},
-//   sulfite: {description: 'Sulfite-Free', id: 401},
-//   treeNut: {description: 'Tree Nut-Free', id: 395},
-//   wheat: {description: 'Wheat-Free', id: 392}
-// };
-
-// const allowedDiet = {
-//   lactoVeg: {description: 'Lacto Vegetarian', id: 388},
-//   ovoVeg: {description: 'Ovo Vegetarian', id: 389},
-//   pesc: {description: 'Pescetarian', id: 390},
-//   vegan: {description: 'Vegan', id: 386},
-//   veg: {description: 'Vegetarian', id: 387},
-//   paleo: {description: 'Paleo', id: 403}
-// };
-
-// function CreateDietLinks() {
-
 // Get request to server, return preferences
-
 function handleForm(event) {
   event.preventDefault();
   console.log('in form')
@@ -55,7 +28,7 @@ function handleForm(event) {
 
 } // end listener for Sign Up button
 
-function showLogIn() {
+function showSignUp() {
   viewResetter();
-  $('.login-view').show();
+  $('.signup-view').show();
 }
