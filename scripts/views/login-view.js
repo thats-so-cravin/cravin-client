a53d36939b290c8bdba69f989ae7521e6d8eeed6
Suzanne Leber <79wabvlw1dt7@opayq.com>
2018-06-27 19:31:31 -0700

    user preference strings now saving to DB

--- scripts/views/login-view.js
+++ scripts/views/login-view.js
@@ -3,44 +3,53 @@
 //Lisener for Sign Up button
 $('#sign-up-button').on('click', handleForm);
 
-const dietPrefs = [
-  // Allergies
-  allowedAllergy[
-    {description: 'Dairy-Free' , id: 396},
-    {description: 'Egg-Free' , id: 397},
-    {description: 'Gluten-Free', id: 393},
-    {description: 'Peanut-Free' , id: 394},
-    {description: 'Seafood-Free' , id: 398},
-    {description: 'Sesame-Free' , id: 399 },
-    {description: 'Soy-Free' , id: 400},
-    {description: 'Sulfite-Free' , id: 401},
-    {description: 'Tree Nut-Free' , id: 395},
-    {description: 'Wheat-Free' , id: 392}
-  ],
+const allowedAllergy = {
+  dairy: {description: 'Dairy-Free', id: 396},
+  egg: {description: 'Egg-Free', id: 397},
+  gluten: {description: 'Gluten-Free', id: 393},
+  peanut: {description: 'Peanut-Free', id: 394},
+  seafood: {description: 'Seafood-Free', id: 398},
+  sesame: {description: 'Sesame-Free', id: 399},
+  soy: {description: 'Soy-Free', id: 400},
+  sulfite: {description: 'Sulfite-Free', id: 401},
+  treeNut: {description: 'Tree Nut-Free', id: 395},
+  wheat: {description: 'Wheat-Free', id: 392}
+};
 
-  // Diets
-  allowedDiet[
-    {description: 'Lacto Vegetarian' , id: 388},
-    {description: 'Ovo Vegetarian' , id: 389},
-    {description: 'Pescetarian' , id: 390},
-    {description: 'Vegan' , id: 386},
-    {description: 'Vegetarian' , id: 387},
-    {description: 'Paleo' , id: 403}
-  ]
-];
+const allowedDiet = {
+  lactoVeg: {description: 'Lacto Vegetarian', id: 388},
+  ovoVeg: {description: 'Ovo Vegetarian', id: 389},
+  pesc: {description: 'Pescetarian', id: 390},
+  vegan: {description: 'Vegan', id: 386},
+  veg: {description: 'Vegetarian', id: 387},
+  paleo: {description: 'Paleo', id: 403}
+};
 
 // function CreateDietLinks() {
 
-  // Get request to server, return preferences
+// Get request to server, return preferences
 
-function handleForm(event, callback) {
+function handleForm(event) {
   event.preventDefault();
   console.log('in form')
+
   let userData = {};
+  let allergyStr = `&${$('#allowedAllergy').on().attr('id')}[]=${$('select option:selected').attr('id')}`;
+
+  let dietStr = `&${$('#allowedDiet').on().attr('id')}[]=${$('select option:selected').attr('id')}`;
+
   userData.username = $('#username').val();
   userData.password = $('#password').val();
-  userData.foodPrefs = $('#food-prefs').val();
+  userData.allowed_allergy = allergyStr;
+  userData.allowed_diet = dietStr;
+
+
   $.post(`${app.ENVIRONMENT.apiUrl}/api/v1/thats-so-cravin`, userData)
-    .then(callback(), console.log('successful form entry'))
+    .then(console.log(allergyStr, dietStr))
     .catch(console.error);
+    console.log(userData);
+
+
+  // and use in API call
+
 } // end listener for Sign Up button
--- scripts/views/search-view.js
+++ scripts/views/search-view.js
@@ -11,9 +11,13 @@ function handleSearch(event) {
   searchData.second_ing = $('#second-ing').val();
   searchData.third_ing = $('#third-ing').val();
   searchData.fourth_ing = $('#fourth-ing').val();
+
+
   let searchStr = `&q=${searchData.first_ing}+${searchData.second_ing}+${searchData.third_ing}+${searchData.fourth_ing}`;
+
   console.log(searchStr);
-  $.get(`http://api.yummly.com/v1/api/recipes?_app_id=78c6217b&_app_key=084d45aa3306778e2ebbc3148fdaab96${searchStr}`, data => {console.log(data)})
+
+  $.get(`http://api.yummly.com/v1/api/recipes?_app_id=78c6217b&_app_key=084d45aa3306778e2ebbc3148fdaab96${searchStr}${allergyStr}${dietStr}`, data => {console.log(data)})
 
     app.Recipe.fetchAll(app.Recipe.loadAll, searchStr);
 
