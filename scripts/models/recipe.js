0f6230d441eae714e2209ee80fab14b8329ce0d6
Tara Johnson <tara@madface.com>
2018-06-28 17:03:13 -0700

    Updated more yummly paths to https

--- scripts/models/recipe.js
+++ scripts/models/recipe.js
@@ -41,7 +41,7 @@ var app = app || {};
     // 084d45aa3306778e2ebbc3148fdaab96&q=onion+soup`)
 
     // dynamic search
-    $.get(`http://api.yummly.com/v1/api/recipes?_app_id=78c6217b&_app_key=084d45aa3306778e2ebbc3148fdaab96${searchStr}`, data => { console.log(data) })
+    $.get(`https://api.yummly.com/v1/api/recipes?_app_id=78c6217b&_app_key=084d45aa3306778e2ebbc3148fdaab96${searchStr}`, data => { console.log(data) })
       .then(results => {
         console.log(results.matches);
         // Recipe.loadAll(results.matches);
--- scripts/views/recipe-view.js
+++ scripts/views/recipe-view.js
@@ -19,7 +19,7 @@ var app = app || {};
       event.preventDefault();
       let id = this.name;
       console.log(this.name)
-      $.get(`http://api.yummly.com/v1/api/recipe/${id}?_app_id=78c6217b&_app_key=084d45aa3306778e2ebbc3148fdaab96`, data => {
+      $.get(`https://api.yummly.com/v1/api/recipe/${id}?_app_id=78c6217b&_app_key=084d45aa3306778e2ebbc3148fdaab96`, data => {
         window.open(data.source.sourceRecipeUrl, '_blank')
       })
     }
